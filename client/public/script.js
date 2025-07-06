f7148d687ad0ca6a370700aeb43dd6c31d0958f7
ach-melissa <infomely05@gmail.com>
2025-07-06 14:47:27 +0200

    new content

--- client/public/script.js
+++ client/public/script.js
@@ -2,32 +2,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/formationdb');
+// Connect to MongoDB (adjust URL if you're using MongoDB Atlas)
+mongoose.connect('mongodb+srv://cedra:<db_password>@cluster1.asir4uv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+// Create Schema
 const FormationSchema = new mongoose.Schema({
   category: String,
   formations: [
     {
       title: String,
-      items: [String]
-    }
-  ]
+      items: [String],
+    },
+  ],
 });
 
+// Model
 const Formation = mongoose.model('Formation', FormationSchema);
 
+// View engine setup
 app.set('view engine', 'ejs');
+
+// Serve static files like CSS/images from "public" folder
 app.use(express.static('public'));
 
+// Show category page when user clicks
 app.get('/formations/:category', async (req, res) => {
-  const category = req.params.category;
-  const data = await Formation.findOne({ category: new RegExp(category, 'i') });
+  const category = req.params.category.toLowerCase(); // e.g. 'informatique'
+  const data = await Formation.findOne({ category: category });
 
   if (!data) {
-    return res.send("No formations found for this category.");
+    return res.send('No formations found for this category.');
   }
 
   res.render('formations', { data });
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// Start server
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
